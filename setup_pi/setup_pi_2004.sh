f433b155438af3799b2310234c36c2e3db5e2341
MZandtheRaspberryPi <46413365+MZandtheRaspberryPi@users.noreply.github.com>
2025-12-11 14:34:16 +0000

    Refactor Docker installation in setup_pi_2004.sh
    Updated Docker installation process to use GPG key and repository configuration.

--- setup_pi/setup_pi_2004.sh
+++ setup_pi/setup_pi_2004.sh
@@ -17,8 +17,25 @@ sudo rm /etc/apt/sources.list/d/ros2.list
 
 sudo apt update
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 sudo usermod -aG docker $USER
 sudo docker pull mzandtheraspberrypi/ros_sim:2024-10-24
 
